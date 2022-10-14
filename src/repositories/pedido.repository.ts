6468d34c1a393bf0c3669135fed23e3fc48759f2
JuanDavidHerrera2125 <juandavidherrera674@gmail.com>
2022-10-14 09:15:26 -0500

    version 1.0 app

---
+++ src/repositories/pedido.repository.ts
@@ -0,0 +1,35 @@
+import {inject, Getter} from '@loopback/core';
+import {DefaultCrudRepository, repository, BelongsToAccessor, HasManyRepositoryFactory, HasOneRepositoryFactory} from '@loopback/repository';
+import {MongoDbDataSource} from '../datasources';
+import {Pedido, PedidoRelations, Persona, Producto} from '../models';
+import {PersonaRepository} from './persona.repository';
+import {ProductoRepository} from './producto.repository';
+
+export class PedidoRepository extends DefaultCrudRepository<
+  Pedido,
+  typeof Pedido.prototype.id,
+  PedidoRelations
+> {
+
+  public readonly persona: BelongsToAccessor<Persona, typeof Pedido.prototype.id>;
+
+  public readonly personas: HasManyRepositoryFactory<Persona, typeof Pedido.prototype.id>;
+
+  public readonly person: BelongsToAccessor<Persona, typeof Pedido.prototype.id>;
+
+  public readonly producto: HasOneRepositoryFactory<Producto, typeof Pedido.prototype.id>;
+
+  constructor(
+    @inject('datasources.mongoDB') dataSource: MongoDbDataSource, @repository.getter('PersonaRepository') protected personaRepositoryGetter: Getter<PersonaRepository>, @repository.getter('ProductoRepository') protected productoRepositoryGetter: Getter<ProductoRepository>,
+  ) {
+    super(Pedido, dataSource);
+    this.producto = this.createHasOneRepositoryFactoryFor('producto', productoRepositoryGetter);
+    this.registerInclusionResolver('producto', this.producto.inclusionResolver);
+    this.person = this.createBelongsToAccessorFor('person', personaRepositoryGetter,);
+    this.registerInclusionResolver('person', this.person.inclusionResolver);
+    this.personas = this.createHasManyRepositoryFactoryFor('personas', personaRepositoryGetter,);
+    this.registerInclusionResolver('personas', this.personas.inclusionResolver);
+    this.persona = this.createBelongsToAccessorFor('persona', personaRepositoryGetter,);
+    this.registerInclusionResolver('persona', this.persona.inclusionResolver);
+  }
+}
